e8ba165e8d5ebcd7983307f4b3ff89f8b048c922
Artemiy <beljakovav@gmail.com>
2018-12-25 17:31:17 +0300

    Flashlight 25.12.2018

---
+++ public/js/setting_sources_page/changeStatusSource.js
@@ -0,0 +1,21 @@
+/**
+ * Модуль изменяющий состояние источника
+ * 
+ * Версия 0.1, дата релиза 24.12.2018
+ */
+
+'use strict';
+
+/**
+ * 
+ * @param {*} объект типа {
+ *      <sourceID>: {
+ *          statusConnection: true/flase,
+ *          shortName: <краткое название>,
+ *          detailedDescription: <полное название> 
+ *      } 
+ *  }
+ */
+export default function changeStatusSource(statusListsSources) {
+    console.log(statusListsSources);
+}
